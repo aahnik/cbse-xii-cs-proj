4614bdb09ad78a1986be364afbc67a534f2da0bd
aahnik <66209958+aahnik@users.noreply.github.com>
2020-11-13 18:42:24 +0530

    live update

--- python/17_dbRecord.py
+++ python/17_dbRecord.py
@@ -1,4 +1,14 @@
-from .sqlTor import SqlTor
+from sqlTor import SqlTor
 
 with SqlTor() as my_con:
-    
+    cursor = my_con.cursor()
+
+    # cursor.execute('''DROP TABLE IF EXISTS employees; 
+    # CREATE TABLE employees(
+    #     ecode integer,
+    #     ename char(20));
+
+    # INSERT INTO employees
+    # VALUES(1,'ji');
+    # ''')
+    # my_con.commit()
--- python/sqlTor.py
+++ python/sqlTor.py
@@ -5,25 +5,18 @@ An utility module that helps to connect to the my sql database
 import mysql.connector
 
 
-HOST = ''
-USER_NAME = ''
-PASS = ''
-DB = ''
-
-
 class SqlTor():
     def __init__(self) -> None:
-        self.conn = mysql.connector.connect(host='HOST',
-                                            user='USER_NAME',
-                                            passwd='PASS',
-                                            database='DB')
+        self.conn = mysql.connector.connect(host='localhost',
+                                            user='newuser',
+                                            passwd='password',
+                                            database='py_sql')
+
     def __enter__(self):
         if self.conn.is_connected():
             return self.conn
         else:
             raise Exception('Not connected to MySQL')
 
-    def __exit__(self):
+    def __exit__(self, exception_type, exception_value, traceback):
         self.conn.close()
-
-        
