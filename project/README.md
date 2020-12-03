09fe1991ba0035a6292f0b0a8bda9ea0e16deb96
aahnik <66209958+aahnik@users.noreply.github.com>
2020-12-03 12:36:03 +0530

    start project

---
+++ project/README.md
@@ -0,0 +1,26 @@
+# marksman
+
+A simple command-line app to manage marks of students.
+
+## Features
+
+- Add multiple students and tests
+- Record, update and delete entries
+- Search for students, tests and marks
+- Visualize the performance of students
+- Email the results to students, with custom template
+
+## Installation
+
+You must have python 3.9 installed on your system.
+
+Install easily via `pip`, Python's official Package Manager.
+
+```shell
+pip install marksman
+```
+
+>Note: Linux and Mac users might have to use `pip3` to invoke pip
+
+## Usage
+
---
+++ project/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+import re
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('py_package/__init__.py', 'r').read(),
+    re.M).group(1)
+
+
+setuptools.setup(
+    name="marksman",
+    version=version,
+    author="Aahnik Daw",
+    author_email="meet.aahnik@gmail.com",
+    description="A simple command-line app to manage marks of students.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aahnik/",
+    packages=setuptools.find_packages(),
+    install_requires=['pytest'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': ['marksman=marksman.cli:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+
+)
