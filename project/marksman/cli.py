9ae3fd137dc86f72c8a772ec53add6353b99fedf
aahnik <66209958+aahnik@users.noreply.github.com>
2020-12-24 23:00:03 +0530

    towards a better cli

--- project/marksman/cli.py
+++ project/marksman/cli.py
@@ -4,19 +4,21 @@ import sys
 from marksman.app import crud_handler, email_handler, visualization_handler
 import logging
 
+
 def main():
     ''' Command line entry point
     '''
 
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('func',
-                        help='what do you want to do',
-                        choices=['crud', 'email', 'visualize'])
-
     parser.add_argument('-v', '--verbose',
                         help='increase output verbosity',
                         action='store_true')
+
+    subparsers = parser.add_subparsers(help='types of functionality')
+
+    crud_parser = subparsers.add_parser('crud')
+    email_parser = subparsers.add_parser('email')
+    vis_parser = subparsers.add_parser('visualize')
 
     if len(sys.argv) == 1:
         print(f'marksman {__version__}')
@@ -29,10 +31,3 @@ def main():
     if args.verbose:
         print('Verbosity turned on')
         logging.basicConfig(level=logging.INFO)
-
-    if args.func == 'crud':
-        crud_handler()
-    elif args.func == 'email':
-        email_handler()
-    elif args.func == 'visualize':
-        visualization_handler()
