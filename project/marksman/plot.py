a62b748b18789458d8278e9a097fea26e4d47bd6
aahnik <66209958+aahnik@users.noreply.github.com>
2020-12-26 22:37:08 +0530

    finish plot full batch performance

--- project/marksman/plot.py
+++ project/marksman/plot.py
@@ -20,7 +20,8 @@ def plot_student_performance(cursor: Cursor, roll: int, exam: int, cache: dict =
     marks = cursor.fetchone()
 
     if not marks:
-        logger.warn('Marks entry does not exist')
+        logger.warn(
+            f'Marks entry does not exist for exam with uid = {exam} and student with roll = {roll} ')
         return
 
     if not cache.get(exam):
@@ -31,13 +32,36 @@ def plot_student_performance(cursor: Cursor, roll: int, exam: int, cache: dict =
     x = ['student', 'highest', 'average']
     y = [marks[0], analysis.get('highest'), analysis.get('average')]
 
-    plt.barh(x, y)
+    plt.bar(x, y, width=0.4)
+
+    logger.info(f'Plottng horizontal bar grapth with \nx = {x} \n\ny = {y}')
 
     for index, value in enumerate(y):
         plt.text(value, index, str(value))
+
+    plt.xlabel('Comparison')
+    plt.ylabel('Marks')
 
     plt.show()
 
 
 def plot_batch_performance(cursor: Cursor, exam: int):
-    print('todo')
+    cursor.execute(
+        ___(f'SELECT student,marks FROM marks WHERE exam={exam}'))
+    marks_list = cursor.fetchall()
+
+    if not marks_list:
+        logger.warning(f'No marks entries exist for exam with uid = {exam}')
+        return
+
+    x = [f'{i[0]}' for i in marks_list]
+    y = [i[1] for i in marks_list]
+
+    plt.stem(x, y)
+
+    logger.info(f'Plotting stem graph with \nx = {x} \n\ny = {y}')
+
+    plt.xlabel('Students')
+    plt.ylabel('Marks')
+
+    plt.show()
