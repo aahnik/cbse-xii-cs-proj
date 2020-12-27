652d996713d65e1ec43dca3ba581dca4ec066bb6
aahnik <66209958+aahnik@users.noreply.github.com>
2020-12-27 09:14:42 +0530

    functions for email configuration

--- project/marksman/utils.py
+++ project/marksman/utils.py
@@ -23,20 +23,21 @@ def handle_choice(choices: dict) -> None:
     # this handler does not resolve prefix collisions, the first come first server
 
     ch_list = []
-    msg = 'Choose your action  '
+    prefix = '[dim]Choose your action[/dim] '
+    msg = prefix
     for ch in choices.keys():
         ch_list.append(ch[0])
-        msg += f'[red]{ch[0]}[/red]{ch[1:]} '
+        msg += f'[reverse]{ch[0]}[/reverse]{ch[1:]} '
     print('\n'+msg)
-    user_choice = input('>>> ').lower().strip()
+    user_choice = input().lower().strip()
     if not user_choice:
-        logger.warn('You did not choose anything.')
+        logger.warn('You did not choose anything')
         return
     for ch in choices.keys():
         if ch.startswith(user_choice):
             func = choices.get(ch)
             return func()
-    logger.warn('Invalid Choice')
+    logger.warn('Invalid Choice ...quitting')
 
 
 def clear_screen():
@@ -56,24 +57,75 @@ def ensure_parent(filename: str) -> None:
 def fill_dummy(students, exams, marks):
     import random
 
-    for i in range(1, 50):
+    for i in range(1, 51):
         students.insert((i, f'stud_{i}', f'{i}@email.com'))
     logger.info('Created 50 dummy students')
 
-    for j in range(1, 10):
+    for j in range(1, 11):
         exams.insert((j, f'exam_{j}'))
     logger.info('Created 10 dummy exams')
 
-    for j in range(1, 10):
-        for i in range(1, 50):
+    for j in range(1, 11):
+        for i in range(1, 51):
             m = random.randint(1, 100)
             marks.insert((i, j, m))
     logger.info('Filled with random dummy marks entries')
 
-def load_csv(students,exams,marks):
+
+def save_email_config(thing, env_var, value):
+    from marksman.settings import GLOBAL_CONFIG_PATH
+
+    text = f'''[red]If you do not want to enter [bold]{thing}[/bold] every time, then save it in [blue]{env_var}[/blue] environment variable.[/red]
+        \nYou can write the following line in your {GLOBAL_CONFIG_PATH} file:\n
+        [center][dim]{env_var}={value}[/dim][/center]
+        \nRead more about configuring your marksman environment https://git.io/JLMFl\n'''
+    logger.warn(text)
+    print(
+        f'Do you want [bold]marksman[/bold] to save your [blue]{thing}[/blue] ? [reverse]Y[/reverse]es or [reverse]n[/reverse]o')
+    choice = input().lower()
+    if choice in 'yes':
+        with open(GLOBAL_CONFIG_PATH, 'a') as f:
+            f.write(f'{env_var}={value}\n')
+        print(f'Saved [bold]{thing}[/bold] to {GLOBAL_CONFIG_PATH}')
+    elif choice in 'no':
+        print('Ok. Not saving')
+    else:
+        print('Invalid Choice ...not saving')
+
+
+def configure_email():
+    from marksman.settings import SENDER_EMAIL, SENDER_AUTH, SMTP_SERVER, SMTP_PORT
+    from marksman.validators import get_email, get_str
+
+    if not SENDER_EMAIL:
+        SENDER_EMAIL = get_email('Enter [bold]sender email[/bold] address: ')
+        save_email_config('email', 'marksman_sender', SENDER_EMAIL)
+    if not SENDER_AUTH:
+        SENDER_AUTH = get_str(
+            f'Enter password or auth-code to login into email account {SENDER_EMAIL}')
+        save_email_config('auth-code', 'marksman_auth', SENDER_AUTH)
+
+    if not SENDER_EMAIL.endswith('@gmail.com'):
+        logger.warn(
+            'SMTP sever url could not be derrived from email address.\n Learn more https://git.io/JLMFl ')
+        SMTP_SERVER = get_str(
+            'Enter SMTP server address of your email provider: ')
+        save_email_config('smtp server', 'marksman_smptp', SMTP_SERVER)
+    if not isinstance(SMTP_PORT, int):
+        logger.warn(
+            'The SMTP Port you have set is not an integer, using default 587')
+        SMTP_PORT = 587
+    if not SMTP_PORT in (587, 2525):
+        logger.warn(
+            'The SMPTP Port you have set is probably incorrect or insecure.')
+        input('Confirm ?')
+
+    return SENDER_EMAIL, SENDER_AUTH, SMTP_SERVER, SMTP_PORT
+
+
+def load_csv(students, exams, marks):
     pass
 
 
-def export_csv(students,exams,marks):
+def export_csv(students, exams, marks):
     pass
-
