84decbd16f3421fca68fdad3b39107bc0720dcb9
aahnik <66209958+aahnik@users.noreply.github.com>
2020-12-28 17:41:05 +0530

    delete pypi.sh

--- project/pypi.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-python3 -m pip install --upgrade pip
-python3 -m pip install setuptools wheel twine
-rm -rf __pycache__
-python3 setup.py sdist bdist_wheel
-twine upload dist/*
-rm -rf build dist *.egg-info
