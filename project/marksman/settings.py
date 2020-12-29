fe45302a5c951f378fef9b77c858b4079e6929d5
aahnik <66209958+aahnik@users.noreply.github.com>
2020-12-29 18:51:02 +0530

    be silent

--- project/marksman/settings.py
+++ project/marksman/settings.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-from rich import print
 
 MARKSMAN_DIR = os.path.expanduser('~/.marksman')
 GLOBAL_CONFIG_PATH = os.path.join(MARKSMAN_DIR, '.env')
@@ -18,10 +17,3 @@ SHOW_PATH = bool(os.getenv('marksman_show_path'))
 SMTP_HOST = os.getenv('marksman_smtp_host')
 SMTP_PORT = int(os.getenv('marksman_smtp_port', 587))
 INST_NAME = os.getenv('marksman_inst')
-
-if LOUD:
-    print(f'''\n\t[red]YOU ARE WORKING IN A LOUD ENVIRONMENT[/red]
-        [dim]DATABASE PATH = {DB_PATH}
-        SENDER EMAIL = {SENDER_EMAIL}
-        SENDER AUTH = {'EXISTS' if SENDER_AUTH else 'EMPTY'}
-        SHOW PATH = {SHOW_PATH} [/dim] \n\n''')
