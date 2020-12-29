170276486515f11adc02dc77fd25612bacc129d9
aahnik <66209958+aahnik@users.noreply.github.com>
2020-12-29 18:49:09 +0530

    modularity wins. Remove repeated pattern

--- project/marksman/models.py
+++ project/marksman/models.py
@@ -1,73 +1,47 @@
-from marksman.db import Modelz
-
-from marksman.validators import get_pos_int, get_str, get_email
-
-
-def _roll():
-    return get_pos_int('Enter [bold]roll number[/bold] of student:')
+import logging
+from marksman.db import DbModelz
 
 
-def _uid():
-    return get_pos_int('Enter the [bold]unique id[/bold] of exam:')
+logger = logging.getLogger(__name__)
 
 
-class Student:
-
-    def __init__(self, students: Modelz) -> None:
-        self.students = students
-        self.roll = _roll()
-        self.object = self.read()
+class Models:
+    def __init__(self, db_modelz: DbModelz, pks_fn: dict, values_fn: dict):
+        self.db_modelz = db_modelz
+        pks = {}
+        for k, v in pks_fn.items():
+            pks[k] = v()
+        self.pks = pks  # dict of pk and value
+        self.values_fn = values_fn  # dict of value_field_name : func
 
     def read(self):
-        return self.students.exists(roll=self.roll)
+        return self.db_modelz.exists(**self.pks)
 
     def create(self):
-        self.students.insert((self.roll, get_str(), get_email()))
+        calculated = []
+        for _, v in self.pks.items():
+            calculated.append(v)
+        for _, fn in self.values_fn.items():
+            val = fn()
+            if not val:
+                logger.warning('You cant keep this field empty!')
+                val = fn()
+                if not val:
+                    logger.warning(
+                        'Quitting... As you are keeping this field empty')
+                    return
+            calculated.append(val)
+        self.db_modelz.insert(tuple(calculated))
 
     def update(self):
-        self.students.update(f'''name="{get_str(default=self.object[1])}",\
-            email="{get_email(default=self.object[2])}"''', roll=self.roll)
+        calculated = {}
+        for k, v in self.values_fn.items():
+            calculated[k] = v()
+        if any(calculated.values()):
+            self.db_modelz.update(calculated, **self.pks)
+        else:
+            logger.warning('You left all values empty > Not updating anything')
 
     def delete(self):
-        self.students.delete(roll=self.roll)
+        self.db_modelz.delete(**self.pks)
 
-
-class Exam:
-    def __init__(self, exams: Modelz) -> None:
-        self.exams = exams
-        self.uid = _uid()
-        self.object = self.read()
-
-    def read(self):
-        return self.exams.exists(uid=self.uid)
-
-    def create(self):
-        self.exams.insert((self.uid, get_str()))
-
-    def update(self):
-        self.exams.update(f'''name="{get_str(default=self.object[1])}"''',
-                          uid=self.uid)
-
-    def delete(self):
-        self.exams.delete(uid=self.uid)
-
-
-class MarksEntry:
-    def __init__(self, marks: Modelz) -> None:
-        self.marks = marks
-        self.roll = _roll()
-        self.uid = _uid()
-        self.object = self.read()
-
-    def read(self):
-        return self.marks.exists(student=self.roll, exam=self.uid)
-
-    def create(self):
-        self.marks.insert((self.roll, self.uid, get_pos_int('Enter marks: ')))
-
-    def update(self):
-        self.marks.update(
-            f"marks={get_pos_int('Enter marks: ')}", student=self.roll, exam=self.uid)
-
-    def delete(self):
-        self.marks.delete(student=self.roll, exam=self.uid)
