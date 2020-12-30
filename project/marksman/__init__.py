1de54badc4197632ee4224eebf8acf087992b18f
aahnik <66209958+aahnik@users.noreply.github.com>
2020-12-30 19:44:22 +0530

    new version 0.6.0

--- project/marksman/__init__.py
+++ project/marksman/__init__.py
@@ -1,6 +1,10 @@
 ''' A command line utitility to manage marks of students.
 Perform CRUD, email results or visualize data.
 Also supports import and export from and to CSV files.
+
+
+Source Code: https://github.com/aahnik/cbse-xii-cs-proj/tree/main/project
+PyPI: https://pypi.org/project/marksman/
 '''
 
-__version__ = "0.5.7"
+__version__ = "0.6.0"
--- project/marksman/cli.py
+++ project/marksman/cli.py
@@ -27,7 +27,7 @@ def parse_commands() -> Namespace:
 
     main_parser = ArgumentParser(
         description='CLI Tool to manage marks of students efficiently',
-        epilog='''For tutorials and documentation visit http://bit.ly/marksman-cli ''',
+        epilog='''For tutorials and documentation visit https://pypi.org/project/marksman/ ''',
         formatter_class=RawTextHelpFormatter)
 
     main_parser.add_argument('-l', '--loud',
